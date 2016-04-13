038e025f911591eff63d4b49c9076060890ce7ae
David Jones <djones6@uk.ibm.com>
2016-04-13 09:27:43 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let package = Package(
+    name: "Kitura-TechEmpower",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 8)
+    ])
+
---
+++ Sources/main.swift
@@ -0,0 +1,55 @@
+import Kitura
+import KituraNet
+import KituraSys
+import SwiftyJSON
+
+let router = Router()
+
+typealias JSONDictionary = [String: Any]
+
+// Return a simple JSON dictionary with one element
+func helloWorldDict() -> JSONDictionary {
+  var result = JSONDictionary()
+  result["message"] = "Hello, World!"
+  return result
+}
+
+// Hello World
+router.get("/hello") {
+request, response, next in
+    response.status(HttpStatusCode.OK).send("Hello, world!")
+    next()
+}
+
+// TechEmpower test 1: JSON serialization
+router.get("/json") {
+request, response, next in
+// Basic implementation
+//    var jsonObject = JSONDictionary()
+//    jsonObject["message"] = "Hello, World!"
+//    var result = JSON(jsonObject)
+// or:
+//    var result = JSON(["message":"Hello, World!"])
+// or:
+    var result = JSON(helloWorldDict())
+    response.setHeader("Server", value: "Kitura-TechEmpower")
+    response.status(HttpStatusCode.OK).sendJson(result)
+    next()
+}
+
+// TechEmpower test 2: Single database query
+router.get("/db") {
+request, response, next in
+    // Get a random row (range 1 to 10,000) from DB: id(int),randomNumber(int)
+    // Convert to object using object-relational mapping (ORM) tool
+    // Serialize object to JSON - example: {"id":3217,"randomNumber":2149}
+
+    var result = JSON(["message":"TODO"])
+    response.status(HttpStatusCode.OK).sendJson(result)
+    next()
+}
+
+
+let server = HttpServer.listen(8090, delegate: router)
+Server.run()
+
