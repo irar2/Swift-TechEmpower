bcd708624b63ae95beda57ccbc717e43854468a3
David Jones <djones6@uk.ibm.com>
2016-05-03 14:48:27 +0100

    build debug or release builds

--- build.sh
+++ build.sh
@@ -6,16 +6,36 @@
 # Linux:
 # sudo apt-get install libhttp-parser-dev libcurl4-openssl-dev libhiredis-dev
 
-swift build --clean
+if [ -z "$1" ]; then
+  echo "Specify build type (release or debug)"
+  exit 1
+fi
 
+# Build type
+case "$1" in
+release)
+  BUILDFLAGS="-c release"
+  ;;
+debug)
+  BUILDFLAGS=""
+  ;;
+*)
+  echo "Build type '$1' is not 'release' or 'debug' - building debug"
+  BUILDFLAGS=""
+esac
+
+# Build flags for Kitura appropriate for current OS
 case `uname` in
 Linux)
-  swift build -Xcc -fblocks -c release
+  KITURA_BUILDFLAGS="-Xcc -fblocks"
   ;;
 Darwin)
-  swift build -Xcc -fblocks -Xswiftc -I/usr/local/include -Xlinker -L/usr/local/lib -c release
+  KITURA_BUILDFLAGS="-Xcc -fblocks -Xswiftc -I/usr/local/include -Xlinker -L/usr/local/lib"
   ;;
 *)
   echo "Unknown OS `uname`"
+  exit 1
 esac
 
+swift build --clean
+swift build $KITURA_BUILDFLAGS $BUILDFLAGS
