16a3aaa679fde4b4a58b29357c4176371a69f62a
David Jones <djones6@uk.ibm.com>
2016-07-05 11:46:53 +0100

    Add initial Vapor implementation based on Vapor 0.12

---
+++ vapor/Sources/main.swift
@@ -0,0 +1,37 @@
+import Vapor
+
+//Log.enabledLevels = [.Error, .Fatal]
+
+let app = Application()
+
+/**
+	This first route will return the welcome.html
+	view to any request to the root directory of the website.
+
+	Views referenced with `app.view` are by default assumed
+	to live in <workDir>/Resources/Views/ 
+
+	You can override the working directory by passing
+	--workDir to the application upon execution.
+*/
+app.get("/") { request in
+	return try app.view("welcome.html")
+}
+
+// TechEmpower test 0: plaintext
+app.get("plaintext") { request in
+    var response = Response(status: .ok, body: "Hello, World!")
+    response.headers["Content-Type"] = "text/plain"
+    return response
+}
+
+// TechEmpower test 1: JSON serialization
+app.get("json") { request in
+    return JSON([
+            "message":"Hello, World!"
+        ])
+}
+
+// Print what link to visit for default port
+print("Visit http://localhost:8080")
+app.start()
