b495796d29e4ae33c94f3b4a2bae9f3fcd274e33
David Jones <djones6@uk.ibm.com>
2016-07-05 12:02:56 +0100

    Add testcase for evaluating cost of multiple routes

---
+++ kitura/Sources/100Routes/main.swift
@@ -0,0 +1,28 @@
+import Kitura
+import KituraNet
+import KituraSys
+
+let router = Router()
+
+func myHandler(request: RouterRequest, response: RouterResponse, next: ()->Void) {
+  response.headers["Content-Type"] = "text/plain";
+  response.status(.OK).send("Hello, world!");
+  next()
+}
+
+func myEndHandler(request: RouterRequest, response: RouterResponse, next: ()->Void) throws {
+  response.headers["Content-Type"] = "text/plain";
+  response.status(.OK).send("Hello, world!");
+  try response.end()
+}
+
+// Evaluate performance penalty of having a large number of routes present
+for i in 0...99 {
+  router.get("/route\(i)", handler: myHandler)
+}
+
+// TechEmpower test 0: plaintext
+router.get("/plaintext", handler: myEndHandler)
+
+Kitura.addHTTPServer(onPort: 8080, with: router)
+Kitura.run()
