01ba8e98d6395c982d9544f704d6f6f1aefefa02
David Jones <djones6@uk.ibm.com>
2016-07-06 15:46:02 +0100

    Fix blackfish to compile on Linux

--- blackfish/Package.swift
+++ blackfish/Package.swift
@@ -3,9 +3,7 @@ import PackageDescription
 let package = Package(
     name: "BlackfishApp",
     dependencies: [
-        .Package(url: "https://github.com/elliottminns/blackfish.git", 
+        .Package(url: "https://github.com/djones6/blackfish.git", 
                  majorVersion: 0, minor: 8),
-//        .Package(url: "https://github.com/elliottminns/blackfish-stencil.git",
-//                 majorVersion: 0, minor: 8)
     ]
 )
