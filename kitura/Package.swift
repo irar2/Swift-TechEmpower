8db6bccb72d6bb7f2e2874f2e1433a6742f89851
David Jones <djones6@uk.ibm.com>
2016-08-18 16:43:12 +0100

    Update to Kitura 0.26

--- kitura/Package.swift
+++ kitura/Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Kitura-TechEmpower",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 25),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 25),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 26),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 26),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 0, minor: 6),
     ])
 
