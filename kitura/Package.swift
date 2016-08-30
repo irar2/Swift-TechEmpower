d5a2fb0577e9d11ae60bc1bded90a967e697c2ef
David Jones <djones6@uk.ibm.com>
2016-08-30 13:04:35 +0100

    Update to Kitura 0.28, 08/23 snapshot, 3.0 API for Dispatch on Linux

--- kitura/Package.swift
+++ kitura/Package.swift
@@ -3,8 +3,9 @@ import PackageDescription
 let package = Package(
     name: "Kitura-TechEmpower",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 26),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 26),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 28),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 28),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 15),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 0, minor: 7),
     ])
 
--- kitura/Sources/SemaphoreTest/main.swift
+++ kitura/Sources/SemaphoreTest/main.swift
@@ -5,25 +5,11 @@ import Dispatch
 
 let router = Router()
 
-#if os(Linux)
-let mySema = dispatch_semaphore_create(2)
-#else
 let mySema = DispatchSemaphore(value: 2)
-#endif
 
 // Test semaphores under load
 router.get("/test") {
 request, response, next in
-#if os(Linux)
-    let ret = dispatch_semaphore_wait(mySema, DISPATCH_TIME_FOREVER)
-    if (ret != 0) {
-      response.status(.badRequest).send("Semaphore returned rc=\(ret)")
-    } else {
-      response.headers["Content-Type"] = "text/plain"
-      response.status(.OK).send("Hello, world!")
-    }
-    dispatch_semaphore_signal(mySema)
-#else
     let ret = mySema.wait(timeout: .distantFuture)
     if (ret == DispatchTimeoutResult.timedOut) {
       response.status(.badRequest).send("Semaphore returned rc=\(ret)")
@@ -32,7 +18,6 @@ request, response, next in
       response.status(.OK).send("Hello, world!")
     }
     mySema.signal()
-#endif
     next()
 }
 
