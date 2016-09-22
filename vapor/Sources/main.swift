299b5f26213ff2bb1ed9d537ddf2d8fa7e01cb06
David Jones <djones6@uk.ibm.com>
2016-09-22 11:31:52 +0100

    Update to Vapor 1.0

--- vapor/Sources/main.swift
+++ vapor/Sources/main.swift
@@ -4,20 +4,6 @@ import HTTP
 let app = Droplet()
 app.log.enabled = [.error, .fatal]
 
-/**
-	This first route will return the welcome.html
-	view to any request to the root directory of the website.
-
-	Views referenced with `app.view` are by default assumed
-	to live in <workDir>/Resources/Views/ 
-
-	You can override the working directory by passing
-	--workDir to the application upon execution.
-*/
-app.get("/") { request in
-	return try app.view("welcome.html")
-}
-
 // TechEmpower test 0: plaintext
 app.get("plaintext") { request in
     var response = Response(status: .ok, body: "Hello, World!")
@@ -27,11 +13,11 @@ app.get("plaintext") { request in
 
 // TechEmpower test 1: JSON serialization
 app.get("json") { request in
-    return try JSON([
+    return try JSON(node: [
             "message":"Hello, World!"
         ])
 }
 
 // Print what link to visit for default port
 print("Visit http://localhost:8080")
-app.serve()
+app.run()
