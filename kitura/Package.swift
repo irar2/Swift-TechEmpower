ecef155bb26e924a3208be212508bff0e8834a00
David Jones <djones6@uk.ibm.com>
2016-10-13 12:02:05 +0100

    Lock Kitura-net to 1.0.0

--- kitura/Package.swift
+++ kitura/Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-TechEmpower",
     dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", Version(1, 0, 0)),
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0),
