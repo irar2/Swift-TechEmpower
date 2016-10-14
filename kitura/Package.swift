a7e44bdc6655dd0e674152c28abff5fdea29f36a
David Jones <djones6@uk.ibm.com>
2016-10-14 12:40:14 +0100

    Remove fixed Kitura-net dependency

--- kitura/Package.swift
+++ kitura/Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Kitura-TechEmpower",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", Version(1, 0, 1)),
+// Uncomment to restrict Kitura-net to a specific version
+//        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", Version(1, 0, 1)),
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0),
