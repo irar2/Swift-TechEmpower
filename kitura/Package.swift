757cf3f21321463bb1ab47f38475cd09c8c61ff9
David Jones <djones6@uk.ibm.com>
2016-11-28 10:31:16 +0000

    Use Kitura 1.x

--- kitura/Package.swift
+++ kitura/Package.swift
@@ -5,9 +5,9 @@ let package = Package(
     dependencies: [
 // Uncomment to restrict Kitura-net to a specific version
 //        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", Version(1, 1, 0)),
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 2, minor: 0),
     ])
 
