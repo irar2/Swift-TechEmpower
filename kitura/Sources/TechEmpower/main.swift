33df85bf62a80db8a2016ebe0935998fffd0039f
David Jones <djones6@uk.ibm.com>
2016-12-20 15:54:29 +0000

    Add Apache license

--- kitura/Sources/TechEmpower/main.swift
+++ kitura/Sources/TechEmpower/main.swift
@@ -1,3 +1,19 @@
+/*
+ * Copyright IBM Corporation 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import Kitura
 import SwiftyJSON
 import LoggerAPI
@@ -10,13 +26,11 @@ let port = Int(ProcessInfo.processInfo.environment["PORT"] ?? "8080") ?? 8080
 
 let router = Router()
 
-// TechEmpower test 0: plaintext
+// TechEmpower test 6: plaintext
 router.get("/plaintext") {
 request, response, next in
     response.headers["Content-Type"] = "text/plain"
     response.status(.OK).send("Hello, world!")
-    // next()
-    // Avoid slowdown walking remaining routes
     try response.end()
 }
 
@@ -24,10 +38,8 @@ request, response, next in
 router.get("/json") {
 request, response, next in
     var result = JSON(["message":"Hello, World!"])
-    response.headers["Server"] = "Kitura-TechEmpower"
+    response.headers["Server"] = "Kitura"
     response.status(.OK).send(json: result)
-    // next()
-    // Avoid slowdown walking remaining routes
     try response.end()
 }
 
