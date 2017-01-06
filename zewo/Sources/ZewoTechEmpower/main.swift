c222197292890c19d6648f15075e3bdc54dad67b
David Jones <djones6@uk.ibm.com>
2017-01-06 09:20:23 +0000

    Add /queries and /updates

--- zewo/Sources/ZewoTechEmpower/main.swift
+++ zewo/Sources/ZewoTechEmpower/main.swift
@@ -36,13 +36,70 @@ let router = BasicRouter() { route in
     }
 
     //
-    // TechEmpower test 2: Single database query
+    // TechEmpower test 2: Single database query (raw, no ORM)
     //
     route.get("/db") { _ in
         let headers: Headers = ["Server": "zewo"]
         var dict = try getRandomRow()
         return Response(headers: headers, content: dict, contentType: .json)
     }   
+
+    //
+    // TechEmpower test 3: Multiple database queries (raw, no ORM)
+    // Get param provides number of queries: /queries?queries=N
+    //
+    route.get("/queries") {
+    request in
+        let headers: Headers = ["Server": "zewo"]
+        var queriesParam = 1
+        for queryItem in request.queryItems {
+            if queryItem.name == "queries" {
+                queriesParam = Int(queryItem.value ?? "1") ?? 1
+                break
+            }
+        }
+        let numQueries = max(1, min(queriesParam, 500))      // Snap to range of 1-500 as per test spec
+        var results: [[String:Int]] = []
+        for i in 1...numQueries {
+            var dict = try getRandomRow()
+            results.append(dict)
+        }
+        // Return JSON representation of array of results
+        return try Response(headers: headers, content: results, contentType: .json)
+    }
+    
+    //
+    // TechEmpower test 4: fortunes (TODO)
+    //
+    route.get("/fortunes") {
+    request in
+        let headers: Headers = ["Server": "zewo"]
+        return Response(status: .badRequest, headers: headers, body: "Unimplemented")
+    }
+    
+    //
+    // TechEmpower test 5: updates (raw, no ORM)
+    //
+    route.get("/updates") {
+    request in
+        let headers: Headers = ["Server": "zewo"]
+        var queriesParam = 1
+        for queryItem in request.queryItems {
+            if queryItem.name == "queries" {
+                queriesParam = Int(queryItem.value ?? "1") ?? 1
+                break
+            }
+        }
+        let numQueries = max(1, min(queriesParam, 500))      // Snap to range of 1-500 as per test spec
+        var results: [[String:Int]] = []
+        for i in 1...numQueries {
+            var dict = try getRandomRow()
+            try updateRow(id: dict["id"]!)
+            results.append(dict)
+        }
+        // Return JSON representation of array of results
+        return try Response(headers: headers, content: results, contentType: .json)
+    }
 
 }
 
